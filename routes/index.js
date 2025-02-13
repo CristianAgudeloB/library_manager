ca545e6999a73c0b0ada7d3234ae1f4ad7bce269
Cristian <cagudelo3245@gmail.com>
2025-02-13 15:14:19 -0500

    fix responsive

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Comic = require('../models/Comic');
+const Comic = require('../models/comic');
 const Series = require('../models/series');
 
 // Ruta para buscar cómics
